6186ea67dbbddb8e160a95f6ae1d53b0777a3e2c
Thilina <thilinalakshanhewage1999@gmail.com>
2023-09-20 17:50:56 +0530

    Implement Traveller calender

--- Gola/src/components/Traveler/Calender.js
+++ Gola/src/components/Traveler/Calender.js
@@ -1,99 +1,34 @@
-// import React, { useState } from "react";
-// import format from "date-fns/format";
-// import {
-//   add,
-//   parse,
-//   eachDayOfInterval,
-//   endOfMonth,
-//   getDay,
-//   isSameMonth,
-//   isToday,
-//   startOfToday,
-// } from "date-fns";
+import React from 'react';
+import { useState , useEffect} from 'react';
+import axios from 'axios';
 
-// import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import {Inject,ScheduleComponent , Day , Week , WorkWeek , Month , Agenda} from '@syncfusion/ej2-react-schedule'
 
-// // Helper function to capitalize the first letter of a string
-// function capitalizeFirstLetter(string) {
-//   return string.charAt(0).toUpperCase() + string.slice(1);
-// }
+const Calender = () => {
+    const [events, setEvents] = useState([]);
 
-// const Calendar = () => {
-//   const [currMonth, setCurrMonth] = useState(() => format(today(), "MMM-yyyy"));
-//   let firstDayOfMonth = parse(currMonth, "MMM-yyyy", new Date());
+    // useEffect(() => {
+    //   // Fetch events from the Express.js API
+    //   axios.get("/events").then((response) => {
+    //     setEvents(response.data);
+    //   });
+    // }, []);
 
-//   const daysInMonth = eachDayOfInterval({
-//     start: firstDayOfMonth,
-//     end: endOfMonth(firstDayOfMonth),
-//   });
+  return (
+    <div className="m-[20px] shadow-lg rounded-[15px]">
+      <ScheduleComponent
+        currentView="Month"
+        className="rounded-[15px]"
+        readonly={true}
+        eventSettings={{ dataSource: events }}
+      >
+        {/* eventSetting use to add events from database */}
+        {/* readonly={true} use to Set the calendar to read-only mode */}
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent>
+    </div>
+  );
+}
 
-//   const today = startOfToday();
+export default Calender;
 
-//   const getPrevMonth = (event) => {
-//     event.preventDefault();
-//     const firstDayOfPrevMonth = add(firstDayOfMonth, { months: -1 });
-//     setCurrMonth(format(firstDayOfPrevMonth, "MMM-yyyy"));
-//   };
-
-//   const getNextMonth = (event) => {
-//     event.preventDefault();
-//     const firstDayOfNextMonth = add(firstDayOfMonth, { months: 1 });
-//     setCurrMonth(format(firstDayOfNextMonth, "MMM-yyyy"));
-//   };
-
-//   const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-
-//   const colStartClasses = [
-//     "",
-//     "col-start-2",
-//     "col-start-3",
-//     "col-start-4",
-//     "col-start-5",
-//     "col-start-6",
-//     "col-start-7",
-//   ];
-
-//   return (
-//     <div>
-//       <div className="flex items-center justify-between">
-//         <p className="text-xl font-semibold">
-//           {format(firstDayOfMonth, "MMMM yyyy")}
-//         </p>
-//         <div className="flex items-center gap-6 justify-evenly sm:gap-12">
-//           <ChevronLeftIcon
-//             className="w-6 h-6 cursor-pointer"
-//             onClick={getPrevMonth}
-//           />
-//           <ChevronRightIcon
-//             className="w-6 h-6 cursor-pointer"
-//             onClick={getNextMonth}
-//           />
-//         </div>
-//       </div>
-//       <div className="grid grid-cols-7 gap-6 mt-8 sm:gap-12 place-items-center">
-//         {days.map((day, idx) => (
-//           <div key={idx} className="font-semibold">
-//             {capitalizeFirstLetter(day)}
-//           </div>
-//         ))}
-//       </div>
-//       <div className="grid grid-cols-7 gap-6 mt-8 sm:gap-12 place-items-center">
-//         {daysInMonth.map((day, idx) => (
-//           <div key={idx} className={colStartClasses[getDay(day)]}>
-//             <p
-//               className={`cursor-pointer flex items-center justify-center font-semibold h-8 w-8 rounded-full hover:text-white ${
-//                 isSameMonth(day, today()) ? "text-gray-900" : "text-gray-400"
-//               } ${!isToday(day) && "hover:bg-blue-500"} ${
-//                 isToday(day) && "bg-red-500 text-white"
-//               }`}
-//             >
-//               {format(day, "d")}
-//             </p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Calendar;
--- Gola/src/screens/Traveler/TrCalender.js
+++ Gola/src/screens/Traveler/TrCalender.js
@@ -1,12 +1,11 @@
 import React from "react";
-import TripCard from "../../components/Traveler/TripCard";
 import Sidebar from "../../components/Traveler/Sidebar";
-import NavbarWithSearch from "../../components/Traveler/TrNavbar";
 import Footer from "../../components/LandingPage/Footer";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import TripNavbarWithSearch from "../../components/Traveler/TripNavbar";
 import TrNavbar from "../../components/Traveler/TrNavbar";
+import Calender from "../../components/Traveler/Calender";
 
 const TrCalendar = () => {
   return (
@@ -16,8 +15,8 @@ const TrCalendar = () => {
         <div className="flex-grow">
           <div>
             <TrNavbar />
-            <div className="grid gap-2 lg:grid-cols-3">
-              <h1>Calender</h1>
+            <div className="">
+              <Calender/>
             </div>
           </div>
         </div>
