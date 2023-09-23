62b563da0fdd85c63003a787d13d815bd91bcb61
Thilina <thilinalakshanhewage1999@gmail.com>
2023-09-23 17:08:41 +0530

    changes places data

--- Gola/src/components/Traveler/PlaceData.js
+++ Gola/src/components/Traveler/PlaceData.js
@@ -1,14 +1,6 @@
 
 const placeList = [
-  {
-    id: 1,
-    name: "Sigiriya",
-      "Sigiriya, also known as the Lion Rock, is a historic site in Sri Lanka built by King Kasyapa in the 5th century. This magnificent fortress palace features a 660-foot high rock, frescoes of beautiful maidens, and extensive water gardens. It stands as a testament to ancient Sri Lankan engineering and artistic prowess.",
-    img: "https://cdn.pixabay.com/photo/2014/09/24/14/40/sigiriya-459197_1280.jpg"
-
-    description:" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed quos natus nisi, iure qui excepturi unde, harum voluptatum quo eligendi facere numquam vel repudiandae neque. Officiis nemo consequuntur id deleniti.",
-
-  },
+ 
   {
     id: 2,
     name: "Mount Lavinia Beach",
